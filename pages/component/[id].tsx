8627f6410083e08f23166aeec7bf9770fa431ad3
Blake Eriks <eblake98@vt.edu>
2022-01-12 08:50:12 -0800

    Fix show page request url

--- pages/component/[id].tsx
+++ pages/component/[id].tsx
@@ -66,7 +66,7 @@ function Show(props: ShowProps) {
 // Define Server Side Props
 export async function getServerSideProps(context: any) {
   // fetch the component, the param was received via context.query.id
-  const res = await fetch(process.env.API_URL + "/" + context.query.id)
+  const res = await fetch(process.env.API_URL + "/components/" + context.query.id)
   const todo = await res.json()
 
   //return the serverSideProps the todo and the url from out env variables for frontend api calls
