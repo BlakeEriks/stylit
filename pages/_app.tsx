dbd80a8f6bb55f2947906823d2508d92e5d66d0d
Blake Eriks <eblake98@vt.edu>
2022-01-12 07:57:05 -0800

    Start creating app structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Header from 'components/Header';
+import { Fragment, useState } from 'react';
+import MetaTags from 'components/MetaTags';
+import Footer from 'components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+  const [user, setUser] = useState<object | undefined>()
+
+  return (
+    <Fragment>
+      <MetaTags />
+      <Header user={user} setUser={setUser}/>
+      <Component {...pageProps} />
+      <Footer/>
+    </Fragment>
+  )
 }
 
 export default MyApp
