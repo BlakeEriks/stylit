06f31712e45658a69e40e09562099c8ce2ebba5b
Blake Eriks <eblake98@vt.edu>
2022-01-12 07:56:35 -0800

    Skeleton components

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="w-full flex justify-between p-5">
+      Created by Blake Eriks
+    </div>
+  )
+}
+
+export default Footer
---
+++ components/Header.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import useSocialAuth from 'utils/auth';
+
+// Define the users props
+interface IndexProps {
+  user: any
+  setUser: Function
+}
+
+const Header = (props: IndexProps) => {
+  
+  const {signInWithGithub} = useSocialAuth()
+
+  const onClick = async () => {
+    const res = await signInWithGithub()
+    console.log(res)
+    props.setUser(res)
+  }
+
+  return (
+    <div className="w-full flex justify-between p-5">
+      <div className="text-4xl font-semibold">
+        Stylit💄
+      </div>
+      <Link href="/component">
+        Browse
+      </Link>
+      <button onClick={onClick} className="bg-red-500 rounded-lg px-2">
+        Sign in
+      </button>
+    </div>
+  )
+}
+
+export default Header
