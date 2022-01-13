eed1e52b54b94346ad2e9dc8e1a1f9872a9b5949
Blake Eriks <eblake98@vt.edu>
2022-01-12 17:02:39 -0800

    lots of progress on skeleton

--- pages/component/create.tsx
+++ pages/component/create.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router"
-import { FormEventHandler, useRef, useState } from "react"
+import { ChangeEvent, FormEventHandler, useRef, useState } from "react"
 import { Component, ComponentType } from "utils/types"
+import { TwitterPicker } from 'react-color';
+import { MenuItem, Popover, Select, Slider } from "@mui/material";
 
 // Define props
 interface CreateProps {
@@ -9,11 +11,19 @@ interface CreateProps {
 
 interface Styles {
   color: string
+  fontSize: string
+  fontWeight: string
+  fontFamily: string
+  letterSpacing: string
   background: string
-  border: string
+  borderColor: string
+  borderWidth: string
   padding: string
   borderRadius: string
+  boxShadow: string
 }
+
+const fontOptions = ["Montserrat", "Open Sans", "Raleway", "Roboto"]
 
 // Define Component
 const Create = (props: CreateProps) => {
@@ -21,21 +31,27 @@ const Create = (props: CreateProps) => {
   // get the next route
   const router = useRouter()
 
-  // since there is just one input we will use a uncontrolled form
-  // const item = useRef<HTMLInputElement>(null)
-
   const [styles, setStyles] = useState<Styles>({
-    color: "black", 
+    color: "#000000",
+    fontSize: "24px",
+    fontWeight: "500",
+    fontFamily: "Montserrat, sans-serif",
+    letterSpacing: "0px",
     background: "white",
-    border: "1px solid black",
+    borderColor: "#000000",
+    borderWidth: "2px",
     padding: "4px 8px",
-    borderRadius: "5px"
+    borderRadius: "5px",
+    boxShadow: "0px 0px 0px 0px #000000"
   })
+
   const [componentType, setComponentType] = useState(0)
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
 
   const onStyleChange = (event: React.ChangeEvent) => {
+    console.log(event)
     const element = event.currentTarget as HTMLInputElement
-    setStyles({...styles, [element.name]: element.value})
+    setStyles({ ...styles, [element.name]: element.value })
   }
 
   // Function to create new component
@@ -43,7 +59,7 @@ const Create = (props: CreateProps) => {
     event.preventDefault()
 
     // construct new component, create variable, check it item.current is not null to pass type checks
-    let component: Component = { creator_id: "61dcce4e2fa77b6e4b654bd7", type: 0, styles: {}, likes: {count: 0, users: []} }
+    let component: Component = { creator_id: "61dcce4e2fa77b6e4b654bd7", type: 0, styles: {}, likes: { count: 0, users: [] } }
 
     // Make the API request
     await fetch(props.url, {
@@ -58,45 +74,277 @@ const Create = (props: CreateProps) => {
     router.push("/component")
   }
 
+  const getPadding = (axis: string) => Number(styles.padding.split(" ")[axis === 'x' ? 1 : 0].replace("px", ""))
+
+  const getShadowFragment = (index: number) => {
+    const fragment = styles.boxShadow.split(" ")[index]
+    if (index === 4) return fragment
+    return fragment.replace("px", "")
+  }
+
+  const setShadow = (value: number | string, index: number) => {
+    const fragments = styles.boxShadow.split(" ")
+    fragments[index] = value + (index !== 4 ? "px" : '')
+    setStyles({ ...styles, boxShadow: fragments.join(" ") })
+  }
+
+  const setColorByAnchoredEl = (hex: string) => {
+    if (!anchorEl) return
+    const name = anchorEl.getAttribute("name")
+    if (!name) return
+    if (name === "shadow") {
+      setShadow(hex, 4)
+    }
+    else {
+      setStyles({ ...styles, [name]: hex })
+    }
+  }
+  console.log(styles)
   return (
     <div className="border w-full h-5/6 p-8 flex flex-col justify-center mx-auto">
       <h1 className="text-center">Create a New Component</h1>
-      {/* Display the component on the left */}
       <div className="flex flex-row">
+
+        {/* Display the component on the left */}
         <div className="w-1/3 flex items-center justify-center">
-          {ComponentType[componentType] === "Button" && 
-            <button style={styles}>Button</button>
+          {ComponentType[componentType] === "Button" &&
+            <button style={styles} className="transition-all duration-75 ease-linear">
+              Button
+            </button>
           }
-          {ComponentType[componentType] === "Input" && 
+          {ComponentType[componentType] === "Input" &&
             <input style={styles} placeholder="input..."></input>
           }
-          {ComponentType[componentType] === "Card" && 
+          {ComponentType[componentType] === "Card" &&
             <button style={styles}></button>
           }
         </div>
+
         {/* Edit the component on the right */}
         <form onSubmit={handleSubmit} className="flex flex-col items-center w-2/3">
           <h1>Edit Component</h1>
-          <div className="my-2">
-            <span className="px-2">Font Color:</span>
-            <input name="color" value={styles.color} onChange={onStyleChange} />
+
+          {/* Font Edit Section */}
+          <div className="my-2 w-full flex flex-col">
+            <div className="px-2 text-center">Font</div>
+            <button type="button"
+              name="color"
+              className="p-4 rounded-lg border-2"
+              style={{ background: styles.color }}
+              onClick={e => setAnchorEl(e.currentTarget)}>
+            </button>
+
+            <div className="flex flex-row w-full justify-center">
+              size
+              <span className="w-2/3 px-4">
+                <Slider
+                  size="small"
+                  max={40}
+                  min={10}
+                  aria-label="Small"
+                  onChange={(e, v) => setStyles({ ...styles, fontSize: `${v}px` })}
+                  value={Number(styles.fontSize.replace("px", ""))}
+                  />
+              </span>
+            </div>
+
+            <div className="flex flex-row w-full justify-center">
+              weight
+              <span className="w-2/3 px-4">
+                <Slider
+                  size="small"
+                  max={900}
+                  min={100}
+                  step={100}
+                  onChange={(e, v) => setStyles({ ...styles, fontWeight: `${v}` })}
+                  value={Number(styles.fontWeight)}
+                  />
+              </span>
+            </div>
+            
+            <div className="flex flex-row w-full justify-center">
+              spacing
+              <span className="w-2/3 px-4">
+                <Slider
+                  size="small"
+                  max={20}
+                  onChange={(e, v) => setStyles({ ...styles, letterSpacing: `${v}px` })}
+                  value={Number(styles.letterSpacing.replace("px",""))}
+                  />
+              </span>
+            </div>
+
+            <Select
+              value={styles.fontFamily.split(",")[0]}
+              onChange={ event => (
+                setStyles({...styles, fontFamily: `${event.target.value}, sans-serif`}))}
+                displayEmpty
+                >
+              {fontOptions.map(font => (
+                <MenuItem 
+                key={font} 
+                value={font}
+                style={{fontFamily: `${font}, sans-serif`, fontWeight: "600"}}
+                >
+                    {font}
+                </MenuItem>
+              ))}
+            </Select>
           </div>
+
+          {/* Background Edit Section */}
           <div className="my-2">
             <span className="px-2">Background:</span>
-            <input name="background" value={styles.background} onChange={onStyleChange}/>
+            <button type="button"
+              name="background"
+              className="p-4 rounded-lg border-2"
+              style={{ background: styles.background }}
+              onClick={e => setAnchorEl(e.currentTarget)}>
+            </button>
           </div>
-          <div className="my-2">
-            <span className="px-2">Padding</span>
-            <input name="padding" value={styles.padding} onChange={onStyleChange}/>
+
+          {/* Padding Edit Section */}
+          <div className="my-2 w-full">
+            <div className="px-2 text-center">Padding</div>
+            <div className="flex flex-row w-full justify-center">
+              x
+              <span className="w-2/3 px-4">
+                <Slider
+                  size="small"
+                  max={40}
+                  aria-label="Small"
+                  onChange={(e, v) => setStyles({ ...styles, padding: `${styles.padding.split(" ")[0]} ${v}px` })}
+                  value={getPadding("x")}
+                />
+              </span>
+            </div>
+            <div className="flex flex-row w-full justify-center">
+              y
+              <span className="w-2/3 px-4">
+                <Slider
+                  size="small"
+                  max={40}
+                  aria-label="Small"
+                  onChange={(e, v) => setStyles({ ...styles, padding: `${v}px ${styles.padding.split(" ")[1]}` })}
+                  value={getPadding("y")}
+                />
+              </span>
+            </div>
           </div>
-          <div className="my-2">
-            <span className="px-2">Border:</span>
-            <input name="border" value={styles.border} onChange={onStyleChange}/>
+
+          {/* Border Edit Section */}
+          <div className="my-2 w-full flex flex-col">
+            <div className="px-2 text-center">Border</div>
+            <button type="button"
+              name="borderColor"
+              className="p-4 rounded-lg border-2"
+              style={{ background: styles.borderColor }}
+              onClick={e => setAnchorEl(e.currentTarget)}>
+            </button>
+            <div className="flex flex-row w-full justify-center">
+              width
+              <span className="w-2/3 px-4">
+                <Slider
+                  size="small"
+                  max={8}
+                  step={0.1}
+                  aria-label="Small"
+                  onChange={(e, v) => setStyles({ ...styles, borderWidth: `${v}px` })}
+                  value={Number(styles.borderWidth.replace("px", ""))}
+                />
+              </span>
+            </div>
+            <div className="flex flex-row w-full justify-center">
+              radius
+              <span className="w-2/3 px-4">
+                <Slider
+                  size="small"
+                  max={30}
+                  onChange={(e, v) => setStyles({ ...styles, borderRadius: `${v}px` })}
+                  value={Number(styles.borderRadius.replace("px", ""))}
+                />
+              </span>
+            </div>
           </div>
-          <div className="my-2">
-            <span className="px-2">Border Radius:</span>
-            <input name="borderRadius" value={styles.borderRadius} onChange={onStyleChange}/>
+
+          {/* Shadow Edit Section */}
+          <div className="my-2 w-full flex flex-col">
+            <div className="px-2 text-center">Shadow</div>
+            <button type="button"
+              name="shadow"
+              className="p-4 rounded-lg border-2"
+              style={{ background: getShadowFragment(4) }}
+              onClick={e => setAnchorEl(e.currentTarget)}>
+            </button>
+            <div className="flex flex-row w-full justify-center">
+              offset x
+              <span className="w-2/3 px-4">
+                <Slider
+                  size="small"
+                  max={25}
+                  min={-25}
+                  onChange={(e, v) => setShadow(Number(v), 0)}
+                  value={Number(getShadowFragment(0))}
+                />
+              </span>
+            </div>
+            <div className="flex flex-row w-full justify-center">
+              offset y
+              <span className="w-2/3 px-4">
+                <Slider
+                  size="small"
+                  max={25}
+                  min={-25}
+                  onChange={(e, v) => setShadow(Number(v), 1)}
+                  value={Number(getShadowFragment(1))}
+                />
+              </span>
+            </div>
+            <div className="flex flex-row w-full justify-center">
+              blur
+              <span className="w-2/3 px-4">
+                <Slider
+                  size="small"
+                  max={25}
+                  onChange={(e, v) => setShadow(Number(v), 2)}
+                  value={Number(getShadowFragment(2))}
+                />
+              </span>
+            </div>
+            <div className="flex flex-row w-full justify-center">
+              size
+              <span className="w-2/3 px-4">
+                <Slider
+                  size="small"
+                  max={25}
+                  min={-5}
+                  onChange={(e, v) => setShadow(Number(v), 3)}
+                  value={Number(getShadowFragment(3))}
+                />
+              </span>
+            </div>
           </div>
+
+          {/* Color Picker Pop Up */}
+          <Popover
+            open={!!anchorEl}
+            anchorEl={anchorEl}
+            onClose={() => setAnchorEl(null)}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+          >
+            <TwitterPicker
+              className="absolute"
+              onChangeComplete={({ hex }) => setColorByAnchoredEl(hex)}
+              triangle="top-left"
+            />
+          </Popover>
         </form>
       </div>
     </div>
