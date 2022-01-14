76cb78bd7f6d308b3bb08fb02a56676fe51262db
Blake Eriks <eblake98@vt.edu>
2022-01-13 17:34:26 -0800

    Structure change + additional customization

--- pages/component/create.tsx
+++ pages/component/create.tsx
@@ -1,11 +1,11 @@
 import { useRouter } from "next/router"
 import { ChangeEvent, FormEventHandler, useRef, useState } from "react"
 import { Component, ComponentType } from "utils/types"
-import { TwitterPicker } from 'react-color';
-import { MenuItem, Popover, Select, Slider } from "@mui/material";
+import { CompactPicker, TwitterPicker } from 'react-color';
+import { MenuItem, Popover, Select, Slider, ToggleButton, ToggleButtonGroup } from "@mui/material";
 
 // Define props
-interface CreateProps {
+interface CreateComponentProps {
   url: string
 }
 
@@ -26,11 +26,10 @@ interface Styles {
 const fontOptions = ["Montserrat", "Open Sans", "Raleway", "Roboto"]
 
 // Define Component
-const Create = (props: CreateProps) => {
+const Create = (props: CreateComponentProps) => {
 
   // get the next route
   const router = useRouter()
-
   const [styles, setStyles] = useState<Styles>({
     color: "#000000",
     fontSize: "24px",
@@ -44,25 +43,18 @@ const Create = (props: CreateProps) => {
     borderRadius: "5px",
     boxShadow: "0px 0px 0px 0px #000000"
   })
-
-  const [componentType, setComponentType] = useState(0)
+  const [componentType, setComponentType] = useState(ComponentType.Button)
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
-
-  const onStyleChange = (event: React.ChangeEvent) => {
-    console.log(event)
-    const element = event.currentTarget as HTMLInputElement
-    setStyles({ ...styles, [element.name]: element.value })
-  }
 
   // Function to create new component
   const handleSubmit: FormEventHandler<HTMLFormElement> = async event => {
     event.preventDefault()
 
-    // construct new component, create variable, check it item.current is not null to pass type checks
-    let component: Component = { creator_id: "61dcce4e2fa77b6e4b654bd7", type: 0, styles: {}, likes: { count: 0, users: [] } }
-
+    // construct new component
+    let component: Component = { creator_id: "61dcce4e2fa77b6e4b654bd7", type: componentType, styles: styles, likes: { count: 0, users: [] } }
+  
     // Make the API request
-    await fetch(props.url, {
+    await fetch(`${props.url}/components`, {
       method: "post",
       headers: {
         "Content-Type": "application/json",
@@ -99,30 +91,17 @@ const Create = (props: CreateProps) => {
       setStyles({ ...styles, [name]: hex })
     }
   }
-  console.log(styles)
+
+  const getComponentType = () => ComponentType[componentType]
+
   return (
-    <div className="border w-full h-5/6 p-8 flex flex-col justify-center mx-auto">
-      <h1 className="text-center">Create a New Component</h1>
-      <div className="flex flex-row">
+    <div className="border w-full h-5/6 p-8 flex flex-col justify-center mx-auto max-w-[1000px]">
+      <h1 className="text-center md:text-teal-600">Create a New Component</h1>
 
-        {/* Display the component on the left */}
-        <div className="w-1/3 flex items-center justify-center">
-          {ComponentType[componentType] === "Button" &&
-            <button style={styles} className="transition-all duration-75 ease-linear">
-              Button
-            </button>
-          }
-          {ComponentType[componentType] === "Input" &&
-            <input style={styles} placeholder="input..."></input>
-          }
-          {ComponentType[componentType] === "Card" &&
-            <button style={styles}></button>
-          }
-        </div>
+      <form onSubmit={handleSubmit} className="flex flex-row">
 
-        {/* Edit the component on the right */}
-        <form onSubmit={handleSubmit} className="flex flex-col items-center w-2/3">
-          <h1>Edit Component</h1>
+        {/* Left Side Edit Panel */}
+        <div className="flex flex-col items-center justify-evenly w-1/3 p-4">
 
           {/* Font Edit Section */}
           <div className="my-2 w-full flex flex-col">
@@ -231,6 +210,63 @@ const Create = (props: CreateProps) => {
               </span>
             </div>
           </div>
+        </div>
+
+        {/* Center Panel Component View */}
+        <div className="w-1/3 flex flex-col items-center">
+          <ToggleButtonGroup
+            value={componentType}
+            exclusive
+            onChange={(e,v) => setComponentType(v)}
+            className="my-4"
+          >
+            <ToggleButton value={ComponentType.Button} aria-label="button">
+              Button
+            </ToggleButton>
+            <ToggleButton value={ComponentType.Input} aria-label="input">
+              Input
+            </ToggleButton>
+            <ToggleButton value={ComponentType.Card} aria-label="card">
+              Card
+            </ToggleButton>
+          </ToggleButtonGroup>
+          
+          <div className="component-container">
+            {ComponentType[componentType] === "Button" &&
+              <button 
+                style={styles} 
+                className="transition-all duration-150 ease-linear">
+                Button
+              </button>
+            }
+            {ComponentType[componentType] === "Input" &&
+              <input 
+                style={styles} 
+                placeholder="input..." 
+                className="transition-all duration-150 ease-linear w-5/6"  
+              />
+            }
+            {ComponentType[componentType] === "Card" &&
+              <div
+                style={styles}
+                className="flex items-center justify-center card"
+              >
+                Card
+              </div>
+            }
+          </div>
+
+          {/* Save Component */}
+          <button 
+            type="submit"
+            className="border rounded-2xl px-6 py-2 my-4"
+          >
+            Save
+          </button>
+        </div>
+
+        {/* Edit the component on the right */}
+        <div className="flex flex-col items-center w-1/3 p-4">
 
           {/* Border Edit Section */}
           <div className="my-2 w-full flex flex-col">
@@ -316,7 +352,7 @@ const Create = (props: CreateProps) => {
               <span className="w-2/3 px-4">
                 <Slider
                   size="small"
-                  max={25}
+                  max={getComponentType() === "Card" ? 10 : 15}
                   min={-5}
                   onChange={(e, v) => setShadow(Number(v), 3)}
                   value={Number(getShadowFragment(3))}
@@ -324,34 +360,32 @@ const Create = (props: CreateProps) => {
               </span>
             </div>
           </div>
+        </div>
 
-          {/* Color Picker Pop Up */}
-          <Popover
-            open={!!anchorEl}
-            anchorEl={anchorEl}
-            onClose={() => setAnchorEl(null)}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'right',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'center',
-            }}
-          >
-            <TwitterPicker
-              className="absolute"
-              onChangeComplete={({ hex }) => setColorByAnchoredEl(hex)}
-              triangle="top-left"
-            />
-          </Popover>
-        </form>
-      </div>
+        {/* Color Picker Pop O */}
+        <Popover
+          open={!!anchorEl}
+          anchorEl={anchorEl}
+          onClose={() => setAnchorEl(null)}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+        >
+          <CompactPicker
+            onChangeComplete={({ hex }) => setColorByAnchoredEl(hex)}
+          />
+        </Popover>
+      </form>
     </div>
   )
 }
 
-// export getStaticProps to provie API_URL to component
+// export getStaticProps to provide API_URL to component
 export async function getStaticProps(context: any) {
   return {
     props: {
