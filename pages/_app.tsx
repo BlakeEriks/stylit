34e4e9013db94269b40d2234fd0f4dc910ce194f
Blake Eriks <eblake98@vt.edu>
2022-01-14 19:41:32 -0800

    Add app container and background

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from 'components/Header';
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import MetaTags from 'components/MetaTags';
 import Footer from 'components/Footer';
 
@@ -10,12 +10,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   const [user, setUser] = useState<object | undefined>()
 
   return (
-    <Fragment>
+    <div className="bg-gradient-to-b from-[#5d0d0d] via-red to-gray flex flex-col items-center">
       <MetaTags />
       <Header user={user} setUser={setUser}/>
       <Component {...pageProps} />
       <Footer/>
-    </Fragment>
+    </div>
   )
 }
 
