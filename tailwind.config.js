dd7a4932f2b52bbed8d975352b65cd2639e2a6dc
Blake Eriks <eblake98@vt.edu>
2022-01-14 19:42:03 -0800

    add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'red': '#DB2438',
+      'gold': '#f5ba31',
+      'black': '#191A19',
+      'gray': '#a1a1aa',
+      'white': '#FFFFFF'
+    },
     extend: {},
   },
   plugins: [],
