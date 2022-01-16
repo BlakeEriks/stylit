47e85a3a8763795560138c0a7112ed715ab91cf8
Blake Eriks <eblake98@vt.edu>
2022-01-15 21:37:02 -0800

    Update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,27 @@
+import Explorer from 'components/Explorer';
+import Hero from 'components/Hero'
 import type { NextPage } from 'next'
-import Head from 'next/head'
+import Features from '../components/Features';
+import { Component } from 'utils/types';
 
-const Home: NextPage = () => {
-
+const Home = ({components}: {components: Component[]}) => {
   return (
-    <div>
-      Index Page
-    </div>
+    <>
+      <Hero />
+      <Features />
+      <Explorer components={components} />
+    </>
   )
+}
+
+// GET PROPS FOR SERVER SIDE RENDERING
+export async function getServerSideProps() {
+  // get component data from API
+  const res = await fetch(process.env.API_URL + "/components" as string)
+  const components = await res.json()
+
+  // return props
+  return {props: { components }}
 }
 
 export default Home
