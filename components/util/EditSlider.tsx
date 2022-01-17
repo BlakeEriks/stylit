43067f19bd4c2dc28f7886b2ebde54cd939fa1c5
Blake Eriks <eblake98@vt.edu>
2022-01-16 16:27:29 -0800

    Create custom slider component

---
+++ components/util/EditSlider.tsx
@@ -0,0 +1,46 @@
+import { Slider } from "@mui/material"
+
+interface EditSliderProps {
+  min?: number
+  max?: number
+  step?: number
+  onChange: (event: Event, value: number | number[]) => void
+  value: number
+}
+
+const EditSlider = ({min, max, step, onChange, value}: EditSliderProps ) => {
+  return (
+    <Slider
+      className="w-3/4"
+      size="small"
+      sx={{
+        '& .MuiSlider-rail': {
+          color: 'var(--black)'
+        },
+        '& .MuiSlider-track': {
+          color: 'var(--red)'
+        },
+        '& .MuiSlider-thumb': {
+          color: 'var(--red)'
+        },
+        '& .MuiSlider-thumb:focus': {
+          boxShadow: '0 0 0 8px rgb(219, 36, 56, 0.2);'
+        },
+        '& .MuiSlider-thumb:hover': {
+          boxShadow: '0 0 0 8px rgb(219, 36, 56, 0.2);'
+        },
+        '& .MuiSlider-thumb:active': {
+          boxShadow: '0 0 0 12px rgb(219, 36, 56, 0.2);'
+        }
+      }}
+      max={max}
+      min={min}
+      step={step}
+      aria-label="Small"
+      onChange={onChange}
+      value={value}
+    />
+  )
+}
+
+export default EditSlider
