308d717a6a90dd481a8579eb2387f5100d6acb2c
Blake Eriks <eblake98@vt.edu>
2022-01-17 11:22:58 -0800

    Skeleton drafts page

---
+++ pages/drafts.tsx
@@ -0,0 +1,11 @@
+
+
+// GET PROPS FOR SERVER SIDE RENDERING
+export async function getServerSideProps() {
+  // get component data from API
+  const res = await fetch(process.env.API_URL + "/components" as string)
+  const components = await res.json()
+
+  // return props
+  return {props: { components }}
+}
