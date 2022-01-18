b2549545279b02ac378d8303c56dab948023c39a
Blake Eriks <eblake98@vt.edu>
2022-01-18 10:41:26 -0800

    change mui theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,31 @@ import Header from 'components/Header';
 import { useState } from 'react';
 import MetaTags from 'components/MetaTags';
 import Footer from 'components/Footer';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 function MyApp({ Component, pageProps }: AppProps) {
   
   const [user, setUser] = useState<object | undefined>()
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#db2438'
+      },
+      secondary: {
+        main: '#f5ba31'
+      }
+    }
+  })
 
   return (
-    <div className="bg-gradient-to-b from-[#5d0d0d] via-red to-gray flex flex-col items-center">
-      <MetaTags />
-      <Header user={user} setUser={setUser}/>
-      <Component {...pageProps} />
-      <Footer/>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="bg-gradient-to-b from-[#5d0d0d] via-red to-gray flex flex-col items-center">
+        <MetaTags />
+        <Header user={user} setUser={setUser}/>
+        <Component {...pageProps} />
+        <Footer/>
+      </div>
+    </ThemeProvider>
   )
 }
 
