4a96e65db97e85fa14d0001fb96a14a64a7d4205
Blake Eriks <eblake98@vt.edu>
2022-01-18 10:40:18 -0800

    Add filters to explorer

--- components/Explorer.tsx
+++ components/Explorer.tsx
@@ -1,15 +1,37 @@
 /** @jsxImportSource @emotion/react */
 import { Interpolation, Theme } from "@emotion/react"
-import Link from "next/link"
+import { useEffect, useState } from "react"
 import { Component, ComponentType } from "utils/types"
 import ExploreHeader from "./ExploreHeader"
 
-const Explorer = ({components}: {components: Component[]}) => {
-  return (
-    <div className="w-full bg-offWhite">
-      <ExploreHeader />
+const Explorer = ({url}: {url: string}) => {
 
-      <Link href="/component/create" passHref><button>Create a New Component</button></Link>
+  const [components, setComponents] = useState<Component[]>([])
+  const [filter, setFilter] = useState({
+    button: true,
+    input: true,
+    card: true
+  })
+  const [loading, setLoading] = useState(false)
+
+  useEffect( () => {
+    const fetchComponents = async () => {
+      if (Object.entries(filter).filter(type => type[1]).length === 0) return {json: () => []}
+      return await fetch(url + `/components?type=${Object.entries(filter).filter(type => type[1]).map( (type,index) => index).join(',')}`)
+    }
+
+    setLoading(true)
+    fetchComponents().then( res => res.json())
+      .then(components => {
+        setComponents(components)
+        setLoading(false)
+      })
+  },[filter])
+
+  return (
+    <div className="flex items-center flex-col p-4 w-full bg-offWhite">
+      <ExploreHeader filter={filter} setFilter={setFilter}/>
+
       {/* MAPPING OVER THE COMPONENTS */}
       <div className="border flex flex-row flex-wrap justify-center">
         {components.map(component => (
