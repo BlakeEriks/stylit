6bd68fee1f6c073b25766e7ab45970991ed2cd55
Blake Eriks <eblake98@vt.edu>
2022-01-18 10:41:49 -0800

    fix server props for just the url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,26 @@
 import Explorer from 'components/Explorer';
 import Hero from 'components/Hero'
-import type { NextPage } from 'next'
 import Features from '../components/Features';
-import { Component } from 'utils/types';
 
-const Home = ({components}: {components: Component[]}) => {
+interface IndexProps{
+  url: string
+}
+
+const Home = ({url}: IndexProps) => {
   return (
     <>
       <Hero />
       <Features />
-      <Explorer components={components} />
+      <Explorer url={url} />
     </>
   )
 }
 
 // GET PROPS FOR SERVER SIDE RENDERING
 export async function getServerSideProps() {
-  // get component data from API
-  const res = await fetch(process.env.API_URL + "/components" as string)
-  const components = await res.json()
-
   // return props
-  return {props: { components }}
+  const url = process.env.API_URL
+  return {props: {url}}
 }
 
 export default Home
