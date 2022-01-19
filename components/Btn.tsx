407d99f86fccfe990f3d923aeedd1c1fcdf7c1ec
Blake Eriks <eblake98@vt.edu>
2022-01-19 11:43:02 -0800

    style btn

--- components/Btn.tsx
+++ components/Btn.tsx
@@ -10,7 +10,7 @@ const Btn: React.FC<ButtonProps> = ({children, active, className, onClick}) => {
   return (
     <button 
       type="button"
-      className={`flex items-center rounded-xl font-body px-3 py-1 transition-all duration-150 ${className}`}
+      className={`flex items-center rounded-xl font-body px-3 py-1 transition-all duration-150 shadow-lg ${className}`}
       onClick={onClick}  
     >
       {children}
