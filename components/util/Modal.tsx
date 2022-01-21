469589fe6cd61542f783f7670d8b3010eae18179
Blake Eriks <eblake98@vt.edu>
2022-01-21 11:37:46 -0800

    Add yes/no option to modal

--- components/util/Modal.tsx
+++ components/util/Modal.tsx
@@ -1,3 +1,4 @@
+import CircularProgress from '@mui/material/CircularProgress';
 import Fade from "@mui/material/Fade";
 import MuiModal from "@mui/material/Modal";
 import Btn from "components/Btn";
@@ -7,11 +8,11 @@ import { useModalState } from "utils/modal";
 const Modal = () => {
 
   const [modalState, setModalState] = useModalState()
-  const {open, title, description, promptLogin} = modalState
+  const {open, title, description, type, options} = modalState
 
   return (
     <MuiModal
-      open={open}
+      open={!!open}
       onClose={() => setModalState({...modalState, open: false})}
       className="flex items-center justify-center"
       // closeAfterTransition
@@ -21,23 +22,44 @@ const Modal = () => {
       // }}
     >
       <Fade in={open}>
-        <div className="bg-grey-700 p-5 rounded-xl w-[35vw]">
-          <h1 className="text-white text-3xl">
-            {title}
-          </h1>
-          <p className="text-md text-grey-400 my-3">
-            {description}
-          </p>
-          {promptLogin && 
-            <div className="flex">
-              <Btn className="bg-sky-500 text-white">
-                Sign In with Google <FaGoogle className="text-xl ml-1 -mr-1" />
-              </Btn>
-              <Btn className="ml-2 bg-gold text-black">
-                Sign In with Github
-                <FaGithub className="text-2xl ml-1 -mt-1 -mr-1" />
-              </Btn>
-            </div>
+        <div className="bg-grey-700 p-5 rounded-xl">
+          {type === "loading" ?
+            <CircularProgress /> :
+            <>
+              <h1 className="text-white text-3xl">
+                {title}
+              </h1>
+              <p className="text-md text-grey-400 my-3">
+                {description}
+              </p>
+              {type === "promptLogin" && 
+                <div className="flex">
+                  <Btn className="bg-sky-500 text-white">
+                    Sign In with Google <FaGoogle className="text-xl ml-1 -mr-1" />
+                  </Btn>
+                  <Btn className="ml-2 bg-gold text-black">
+                    Sign In with Github
+                    <FaGithub className="text-2xl ml-1 -mt-1 -mr-1" />
+                  </Btn>
+                </div>
+              }
+              {type === "yesOrNo" &&
+                <div className="flex flex-row justify-end w-full text-lg">
+                  <Btn 
+                    onClick={() => options?.onYes()}
+                    className="rounded-2xl bg-green-500 text-white hover:shadow-lg mr-2"
+                  >
+                    {options?.yesText ||  'Yes'}
+                  </Btn>
+                  <Btn 
+                    onClick={() => options?.onNo()}
+                    className="rounded-2xl bg-red text-white hover:shadow-lg"
+                  >
+                    {options?.noText || "No"}
+                  </Btn>
+                </div>
+              }
+            </>
           }
         </div>
       </Fade>
