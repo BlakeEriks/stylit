29a2296e4a716b653149c7d11e11b0165d288a59
Blake Eriks <eblake98@vt.edu>
2022-01-21 11:39:31 -0800

    define type for modal state

--- utils/modal.ts
+++ utils/modal.ts
@@ -1,8 +1,21 @@
 import { atom, useRecoilState } from 'recoil'
 
+interface ModalInterface {
+  open?: boolean
+  title?: string
+  description?: string
+  type?: string | null
+  options?: {
+    yesText: string
+    noText: string
+    onYes: Function
+    onNo: Function
+  } | null
+}
+
 const modalState = atom({
     key: 'modalState',
-    default: {open: false, title: "", description: "", promptLogin: false}
+    default: {open: false, title: "", description: "", type: null, options: null} as ModalInterface
 })
 
 export const useModalState = () => {
