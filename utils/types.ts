8863bce7eb07c63000117f9b370472a460ef0d3c
Blake Eriks <eblake98@vt.edu>
2022-01-21 11:39:39 -0800

    add transition style

--- utils/types.ts
+++ utils/types.ts
@@ -45,6 +45,7 @@ export interface Styles {
   borderRadius?: string
   boxShadow?: string
   outline?: string
+  transition?: string
 }
 
 export interface StyleGroups {
@@ -70,5 +71,6 @@ export const defaultStyles: Styles = {
   padding: "4px 8px",
   borderRadius: "5px",
   boxShadow: "0px 0px 0px 0px rgba(0,0,0,1)",
-  outline: "none"
+  outline: "none",
+  transition: "all 0.3s ease"
 }
