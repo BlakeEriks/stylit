29758f8b1b8bf07a826f34302996a482486dd6d1
Blake Eriks <eblake98@vt.edu>
2022-01-22 21:23:23 -0800

    Create features component

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,7 +1,77 @@
+import {
+  FiAnchor
+} from "react-icons/fi";
+
 const Features = () => {
+
+  const features = [
+    {
+      title: "100s of Components",
+      description: "Browse hundreds of components to find the perfect fit for your app.",
+      icon: <FiAnchor />
+    },
+    {
+      title: "100s of Components",
+      description: "Browse hundreds of components to find the perfect fit for your app.",
+      icon: <FiAnchor />
+    },
+    {
+      title: "100s of Components",
+      description: "Browse hundreds of components to find the perfect fit for your app.",
+      icon: <FiAnchor />
+    },
+    {
+      title: "100s of Components",
+      description: "Browse hundreds of components to find the perfect fit for your app.",
+      icon: <FiAnchor />
+    },
+    {
+      title: "100s of Components",
+      description: "Browse hundreds of components to find the perfect fit for your app.",
+      icon: <FiAnchor />
+    },
+    {
+      title: "100s of Components",
+      description: "Browse hundreds of components to find the perfect fit for your app.",
+      icon: <FiAnchor />
+    },
+    {
+      title: "100s of Components",
+      description: "Browse hundreds of components to find the perfect fit for your app.",
+      icon: <FiAnchor />
+    },
+    {
+      title: "100s of Components",
+      description: "Browse hundreds of components to find the perfect fit for your app.",
+      icon: <FiAnchor />
+    },
+    {
+      title: "100s of Components",
+      description: "Browse hundreds of components to find the perfect fit for your app.",
+      icon: <FiAnchor />
+    },
+    {
+      title: "100s of Components",
+      description: "Browse hundreds of components to find the perfect fit for your app.",
+      icon: <FiAnchor />
+    },
+  ]
+
   return (
-    <div className="flex items-center justify-center text-6xl h-80 border border-red w-5/6">
-      FEATURES SUPER COOL
+    <div className="flex flex-wrap justify-between">
+      {features.map(({icon, title, description}) => (
+        <div className="flex flex-col items-center flex-wrap min-w-[10vw] max-w-[15vw] mx-4 my-10" data-aos="fade-left">
+          <div className="rounded-lg bg-gradient-to-b from-fuchsia-600 via-pink-700 to-sky-600 text-white p-2 text-6xl">
+            {icon}
+          </div>
+          <div className="text-white text-xl text-center mt-4 mb-2 font-semibold">
+            {title}
+          </div>
+          <div className="text-gray text-base text-center">
+            {description}
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
