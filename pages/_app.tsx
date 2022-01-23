04dff2fc6c16413085f64ce8758488d68c5c7a66
Blake Eriks <eblake98@vt.edu>
2022-01-22 21:24:01 -0800

    remove mui theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,32 @@
-import { createTheme, ThemeProvider } from '@mui/material';
+import "animate.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import Footer from 'components/Footer';
 import Header from 'components/Header';
 import MetaTags from 'components/MetaTags';
 import Modal from 'components/util/Modal';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { RecoilRoot } from 'recoil';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#db2438'
-      },
-      secondary: {
-        main: '#f5ba31'
-      }
-    }
-  })
+
+  useEffect( () => {
+    Aos.init()
+  }, [])
 
   return (
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <div className="bg-gradient-to-bl from-pink-900 via-grey-800 to-yellow-600 flex flex-col items-center">
-          <MetaTags />
-          <Header />
-          <Component {...pageProps} />
-          <Footer/>
-        </div>
-        <Modal />
-        <Toaster position="bottom-right" reverseOrder={false} />
-      </ThemeProvider>
+      <div className="bg-gradient-to-bl from-pink-900 via-grey-800 to-yellow-600 flex flex-col items-center">
+        <MetaTags />
+        <Header />
+        <Component {...pageProps} />
+        <Footer/>
+      </div>
+      <Modal />
+      <Toaster position="bottom-right" reverseOrder={false} />
     </RecoilRoot>
   )
 }
