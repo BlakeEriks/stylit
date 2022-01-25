3f5f97d7925cd54e3816928d88cdc455157fdb09
Blake Eriks <eblake98@vt.edu>
2022-01-25 15:58:13 -0800

    Remove custome mui style

--- components/util/EditSlider.tsx
+++ components/util/EditSlider.tsx
@@ -13,17 +13,6 @@ const EditSlider = ({min, max, step, onChange, value}: EditSliderProps ) => {
     <Slider
       className="w-3/4 transition-all duration-75"
       size="small"
-      sx={{
-        '& .MuiSlider-rail': {
-          color: '#3d5eff'
-        },
-        '& .MuiSlider-track': {
-          color: '#3d5eff'
-        },
-        '& .MuiSlider-thumb': {
-          color: '#3d5eff'
-        },
-      }}
       max={max}
       min={min}
       step={step}
