95c994937813ab555ba969b2b557c94cfd7e6b1a
Blake Eriks <eblake98@vt.edu>
2022-01-24 21:32:38 -0800

    Add likes + bookmarks

--- components/Explorer.tsx
+++ components/Explorer.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { Interpolation, Theme } from "@emotion/react";
-import { BookmarkBorderOutlined } from "@mui/icons-material";
+import { Bookmark, BookmarkBorderOutlined } from "@mui/icons-material";
 import { useEffect, useState } from "react";
 import { FaHeart } from "react-icons/fa";
 import { FiHeart } from "react-icons/fi";
@@ -19,6 +19,7 @@ const Explorer = () => {
   })
   const [loading, setLoading] = useState(false)
   const [likedComponents, setLikedComponentes] = useState<string[]>([])
+  const [bookmarks, setBookmarks] = useState<string[]>([])
   const {user} = useUserState()
 
   useEffect( () => {
@@ -39,22 +40,49 @@ const Explorer = () => {
     if (window.localStorage.getItem("likes")) {
       setLikedComponentes(JSON.parse(window.localStorage.getItem("likes")!))
     }
+    if (window.localStorage.getItem("bookmarks")) {
+      setBookmarks(JSON.parse(window.localStorage.getItem("bookmarks")!))
+    }
   }, [])
 
-  const updateLikes = (component: PublishedComponent) => {
-
+  const updateLikes =  (componentId: String, like: number) => {
+    const index = components.findIndex(component => component._id === componentId)
+    const newComponent = {...components[index]} as PublishedComponent
+    newComponent.likes! += like
+    fetch(`api/components/${componentId}`, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newComponent),
+    })
+    setComponents([...components.slice(0, index), newComponent, ...components.slice(index+1)])
   }
 
   const toggleLike = (componentId: string) => {
     let newLikes: string[] = []
     if (likedComponents.some(liked => liked === componentId)) {
       newLikes = [...likedComponents.filter(liked => liked !== componentId)]
+      updateLikes(componentId, -1)
     }
     else {
       newLikes = [...likedComponents, componentId]
+      updateLikes(componentId, 1)
     }
     setLikedComponentes(newLikes)
     window.localStorage.setItem("likes", JSON.stringify(newLikes))
+  }
+  
+  const toggleBookmark = (componentId: string) => {
+    let newBookmarks: string[] = []
+    if (bookmarks.some(bookmark => bookmark === componentId)) {
+      newBookmarks = [...bookmarks.filter(bookmark => bookmark !== componentId)]
+    }
+    else {
+      newBookmarks = [...bookmarks, componentId]
+    }
+    setBookmarks(newBookmarks)
+    window.localStorage.setItem("bookmarks", JSON.stringify(newBookmarks))
   }
 
   return (
@@ -64,10 +92,10 @@ const Explorer = () => {
       {/* MAPPING OVER THE COMPONENTS */}
       <div className="flex flex-row flex-wrap justify-evenly">
         {components.map(component => (
-          <div key={component._id} className="component-card transition-all duration-150 ease-linear hover:scale-105 group relative">
-            <div className="text-center text-lg text-black  w-full">
+          <div key={component._id} className="component-card transition-all duration-150 ease-linear hover:scale-105 group relative border border-grey-400">
+            {/* <div className="text-center text-lg text-black  w-full">
               {component.name}
-            </div>
+            </div> */}
             <div className="component-container flex-col shadow-inner">
               {ComponentType[component.type] === ComponentType[ComponentType.Button] && 
                 <button
@@ -93,30 +121,35 @@ const Explorer = () => {
                 </div>
               }
             </div>
-            <div className="flex justify-end w-full absolute -bottom-2.5 opacity-0 group-hover:opacity-100 group-hover:bottom-2 transition-all duration-200">
-              <div className="w-11/12 flex flex-col items-end bg-white border-t border-l border-opacity-60 bg-opacity-90 pt-2 px-4 rounded-tl-full text-grey-500 ">
-                <div className="font-semibold flex items-center">
-                  <span 
-                    className="text-pink-400 text-xl cursor-pointer rounded-full mb-1 mr-1 p-1 hover:bg-grey-200 transition-all duration-100" 
-                    onClick={() => toggleLike(component._id!)}
-                  >
-                    {likedComponents.includes(component._id || '') ?
-                      <FaHeart className="animate__animated animate__heartBeat"/>
-                    :
-                      <FiHeart />
-                    }
-                  </span>
-                  <span className="flex items-center">
+            <div className="flex flex-col justify-center w-full border-t border-grey-400 rounded-b-xl bg-grey-200">
+                <div className="flex w-full justify-between p-2">
+                  <div className="flex items-center">
+                    <span 
+                      className="text-pink-300 text-xl cursor-pointer rounded-full mb-1 mr-1 p-1 hover:scale-125 transition-all duration-200" 
+                      onClick={() => toggleLike(component._id!)}
+                    >
+                      {likedComponents.includes(component._id || '') ?
+                        <FaHeart className="animate__animated animate__heartBeat"/>
+                      :
+                        <FiHeart />
+                      }
+                    </span>
                     {component.likes} likes
+                  </div>
+                  <span 
+                    className="rounded-full p-1 cursor-pointer hover:scale-125 transition-all duration-200"
+                    onClick={() => toggleBookmark(component._id!)}
+                  > 
+                    {bookmarks.includes(component._id!) ?
+                      <Bookmark  className="text-[#F5BA31] animate__animated animate__heartBeat"/>
+                    :
+                      <BookmarkBorderOutlined className="text-gray"/>
+                    }
                   </span>
                 </div>
                 <div className="text-sm text-center">
                   Created by {component.creatorId.displayName}
                 </div>
-              </div>
-            </div>
-            <div className="absolute top-10 left-4 rounded-full border p-2 text-lg opacity-0 group-hover:opacity-100 transition-all duration-200 cursor-pointer">
-              <BookmarkBorderOutlined />
             </div>
           </div>
         ))}
