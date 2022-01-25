9ee6453cc3c3539f8209e019a94eee018953beed
Blake Eriks <eblake98@vt.edu>
2022-01-25 15:59:51 -0800

    Error handling + toast notifications

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,12 +1,15 @@
 import { AuthProvider, GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
 import { useEffect } from 'react';
+import toast from "react-hot-toast";
 import Firebase from "./firebase";
+import { useModalState } from './modal';
 import { useUserState } from './user';
 var _ = require('lodash');
 
 const useSocialAuth = () => {
 
   const {setUser, resetUser} = useUserState()
+  const [modalState, setModalState] = useModalState()
 
   useEffect(() => {
     if (window.localStorage.getItem("user")) {
@@ -19,6 +22,12 @@ const useSocialAuth = () => {
     .then( async (userCredential) => {
       
       const user = JSON.parse(JSON.stringify(userCredential.user))
+      if (!user) {
+        setModalState({open: false})
+        toast.error("Failed to sign in.")
+        return
+      }
+
       let dbUser = await (await fetch(`/api/users?email=${user.email}`)).json()
       
       /* If user isn't in our system, create a new one */
@@ -37,10 +46,14 @@ const useSocialAuth = () => {
 
       const combinedUserData = {id: dbUser._id, displayName: user.displayName, photoURL: user.photoURL}
       setUser(combinedUserData);
+      setModalState({open: false})
+      toast.success("Signed in to 💄stylit")
       window.localStorage.setItem("user", JSON.stringify(combinedUserData))
     })
     .catch((err) => {
-      console.log(err);
+      console.log(err)
+      setModalState({open: false})
+      toast.error("Failed to sign in.")
     });
 
   // sign out function
