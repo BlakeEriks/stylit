86dd4782f582ba852467bc2081d860e139f9f1ce
Blake Eriks <eblake98@vt.edu>
2022-01-27 11:14:43 -0800

    Adjust media queries slightly

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,22 @@ module.exports = {
     fontFamily: {
       'body': 'Roboto, sans-serif'
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '900px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1320px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [],
