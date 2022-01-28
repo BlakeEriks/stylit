6759ff91b4b3d407189245c1486b4223027dbb7d
Blake Eriks <eblake98@vt.edu>
2022-01-28 09:56:38 -0800

    Create link button

--- components/Btn.tsx
+++ components/Btn.tsx
@@ -3,18 +3,32 @@ import { MouseEventHandler } from "react"
 interface ButtonProps {
   active?: boolean
   className? : string
-  onClick? : MouseEventHandler<HTMLButtonElement>
+  onClick? : MouseEventHandler<HTMLElement>
+  href?: string
 }
 
-const Btn: React.FC<ButtonProps> = ({children, active, className, onClick}) => {
+const Btn: React.FC<ButtonProps> = ({children, active, className, onClick, href}) => {
+
   return (
-    <button 
-      type="button"
-      className={`flex items-center rounded-lg font-body px-3 py-1 transition-all duration-150 shadow-lg ${className}`}
-      onClick={onClick}  
-    >
-      {children}
-    </button>
+    !!href ?
+      <a 
+        type="button"
+        className={`flex items-center rounded-lg font-body px-3 py-1 transition-all duration-150 shadow-lg ${className}`}
+        onClick={onClick}
+        href={href}
+        target="_blank"
+        rel="noreferrer"
+      >
+        {children}
+      </a>
+    :
+      <button 
+        type="button"
+        className={`flex items-center rounded-lg font-body px-3 py-1 transition-all duration-150 shadow-lg ${className}`}
+        onClick={onClick}  
+      >
+        {children}
+      </button>
   )
 }
 
