cb8bf82fe650a4c403f1e3a68741e2bfbaee86bd
Blake Eriks <eblake98@vt.edu>
2022-02-16 18:40:24 -0800

    File name change

---
+++ pages/components.tsx
@@ -0,0 +1,13 @@
+import SubHero from 'components/SubHero';
+import Explorer from '../components/Explorer';
+
+const Components = () => {
+  return (
+    <>
+      <SubHero />
+      <Explorer />
+    </>
+  )
+}
+
+export default Components
