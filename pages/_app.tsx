990a1b629bfa55ec6ad28a39bbafeb38a8874db0
Blake Eriks <eblake98@vt.edu>
2022-02-16 18:40:05 -0800

    Move Stylit to components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import "animate.css";
 import "aos/dist/aos.css";
+import Stylit from "components/Stylit";
 import Modal from 'components/util/Modal';
 import type { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
 import { RecoilRoot } from 'recoil';
 import '../styles/globals.css';
-import Stylit from "./stylit";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
